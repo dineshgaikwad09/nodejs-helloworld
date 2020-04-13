0fe23b30943babe9e2e80c5a5e69b4c63fa7091f
Dinesh Amarsing Gaikwad <dgaikwad@dminc.com>
2020-04-13 17:13:51 +0530

    modified

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function(req, res) {
  res.send('Welcome to Demo App!!');
 });
 
-app.listen(3000, function() {
- console.log("Server is running at 3000 port!");
+app.listen(8080, function() {
+ console.log("Server is running at 8080 port!");
 });
