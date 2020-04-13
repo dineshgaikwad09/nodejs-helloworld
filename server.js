0f920ddf8feda022b8521812978e563c20e990c2
Dinesh Amarsing Gaikwad <dgaikwad@dminc.com>
2020-04-13 17:43:19 +0530

    Modified

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
- res.send('Welcome to Demo App!!');
+ res.send('Demo App!!');
 });
 
 app.listen(8080, function() {
