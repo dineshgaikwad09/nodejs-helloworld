57e1da7d9d66a71a2216594a7ee0d93308cda188
Dinesh Amarsing Gaikwad <dgaikwad@dminc.com>
2020-04-13 17:54:49 +0530

    modified

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
-app.get('/', function(req, res) {
- res.send('Demo App!!');
-});
+// app.get('/', function(req, res) {
+//  res.send('Demo App!!');
+// });
 
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+  
 app.listen(8080, function() {
  console.log("Server is running at 8080 port!");
 });
